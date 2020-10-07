ab8e526e5ae81504259782fe860c452e5556e0c8
SanadViilor <bence.kiraly7@gmail.com>
2020-10-07 17:04:09 +0200

    added mp3->wav conversion script for CommonVoice

---
+++ mp3_to_wav.py
@@ -0,0 +1,12 @@
+# RUN THIS FILE FOR CONVERTING COMMONVOICE MP3 CLIPS TO WAV
+import os
+from pydub import AudioSegment
+
+PATH_TO_AUDIO = '../de/clips/'
+j = 0
+for i in os.listdir(PATH_TO_AUDIO):
+	sound = AudioSegment.from_mp3(PATH_TO_AUDIO + str(i))
+	sound.export(PATH_TO_AUDIO + i.split('mp3')[0] + 'wav', format="wav")
+	j = j + 1
+	print('Converted ' + str(j) + ' mp3s to wav.')
+	os.remove(PATH_TO_AUDIO + str(i))
