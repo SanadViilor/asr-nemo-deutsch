477ea924882fba0ee79f7a85b857a6f30c53a82c
SanadViilor <bence.kiraly7@gmail.com>
2020-10-13 16:54:49 +0200

    created librispeech manifests

--- librispeech/flac_to_wav.py
+++ librispeech/flac_to_wav.py
@@ -1,1 +1,39 @@
-#TODO
+
+import os
+from glob import glob
+from pydub import AudioSegment
+
+TRAIN_PATH = '../../LibriSpeech/train-clean-100/'
+TEST_PATH = '../../LibriSpeech/test-clean/'
+
+test_sounds = [y for x in os.walk(TEST_PATH) for y in glob(os.path.join(x[0], '*.flac'))]
+train_sounds = [y for x in os.walk(TRAIN_PATH) for y in glob(os.path.join(x[0], '*.flac'))]
+
+print(str(len(test_sounds)) + ' TEST SOUNDS IN TOTAL.')
+print(str(len(train_sounds)) + ' TRAIN SOUNDS IN TOTAL.')
+
+print(test_sounds[121])
+print(test_sounds[1113])
+print(train_sounds[1121])
+testc = 0
+trainc = 0
+for i in test_sounds:
+	sound = AudioSegment.from_file(i, format="flac")
+	k = i.split('flac')[0] + 'wav'
+	print(k)
+	sound.export(k, format="wav")
+	os.remove(i)
+	testc = testc + 1
+	print('Converted ' + str(testc) + ' out of ' + str(len(test_sounds)) + ' test samples.')
+
+
+for i in train_sounds:
+        sound = AudioSegment.from_file(i, format="flac")
+        k = i.split('flac')[0] + 'wav'
+        print(k)
+        sound.export(k, format="wav")
+        os.remove(i)
+        trainc = trainc + 1
+        print('Converted ' + str(trainc) + ' out of ' + str(len(train_sounds)) + ' train samples.')
+
+
--- mp3_to_wav.py
+++ mp3_to_wav.py
@@ -4,10 +4,15 @@ from pydub import AudioSegment
 
 PATH_TO_AUDIO = '../de/clips/'
 j = 0
-print('THERE ARE ' + str(len(os.listdir(PATH_TO_AUDIO))) + 'SAMPLES TO CONVERT.')
-for i in os.listdir(PATH_TO_AUDIO):
-	sound = AudioSegment.from_mp3(PATH_TO_AUDIO + str(i))
-	sound.export(PATH_TO_AUDIO + i.split('mp3')[0] + 'wav', format="wav")
-	j = j + 1
-	print('Converted ' + str(j) + ' mp3s to wav.')
-	os.remove(PATH_TO_AUDIO + str(i))
+s=0
+print('THERE ARE ' + str(len(os.listdir(PATH_TO_AUDIO))) + ' SAMPLES TO CONVERT.')
+files = os.listdir(PATH_TO_AUDIO)
+for i in files:
+	s=s+1
+	print('scanned '+str(s))
+	if (i.split('mp3')[0]) not in files:
+		sound = AudioSegment.from_mp3(PATH_TO_AUDIO + str(i))
+		sound.export(PATH_TO_AUDIO + i.split('mp3')[0] + 'wav', format="wav")
+		j = j + 1
+		print('Converted ' + str(j) + ' mp3s to wav.')
+		os.remove(PATH_TO_AUDIO + str(i))
